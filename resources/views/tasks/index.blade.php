c7ee5c9dd29dcbe1813294188e15efe8a68c8daa
YM318 <yana_malek@yahoo.com>
2024-07-17 15:33:43 +0800

    table to datatable

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,23 +1,30 @@
 
 @extends('layouts.app')
 
+@section('head')
+<link rel="stylesheet" href="//cdn.datatables.net/2.0.8/css/dataTables.dataTables.min.css">
+@endsection
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">Tasks</div>
 
                 <div class="card-body">
                    <table class="table" id="myTable">
-                    <tr>
-                        <td>Bil</td>
-                        <td>Title</td>
-                        <td>User</td>
-                        <td>Due Date</td>
-                        <td>Action</td>
-                    </tr>
-                    @foreach ($tasks as $task)
+                    <thead>
+                        <tr>
+                            <th>Bil</th>
+                            <th>Title</th>
+                            <th>User</th>
+                            <th>Due Date</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($tasks as $task)
                         <tr>
                             <td>
                                 {{ $loop->iteration }}
@@ -36,16 +43,23 @@
                             </td>
 
                             <td>
-                                <a href="{{ route('tasks.show', ['task'=>$task->uuid])}}" class="btn btn-primary">show</a>
+                                <a href="{{ route('tasks.show', ['task'=>$task->uuid])}}" class="btn btn-primary">Show</a>
                             </td>
                         </tr>
                     @endforeach
-
+                    </tbody>
                    </table>
                 </div>
             </div>
         </div>
     </div>
 </div>
+@endsection
+
+@section('script')
+<script src="//cdn.datatables.net/2.0.8/js/dataTables.min.js"></script>
+<script>
+    $('#myTable').DataTable();
+</script>
 @endsection
 
