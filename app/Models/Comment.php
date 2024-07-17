cdd9ac3e372d631a47aa8023647c4b26c12527db
YM318 <yana_malek@yahoo.com>
2024-07-17 09:24:06 +0800

    task model & comment

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -5,13 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Task extends Model
+class Comment extends Model
 {
     use HasFactory;
 
     public $timestamps = true;
 
-    protected $table = 'tasks';
+    protected $table = 'comments';
 
     protected $primaryKey = 'id';
 
@@ -21,15 +21,6 @@ class Task extends Model
 
     // protected $fillable = ['fillable'];
 
-    /**
-     * Get the user that owns the Task
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
-     */
-    public function user()
-    {
-        return $this->belongsTo(User::class, 'user_id', 'id');
-    }
-
+   
 
 }
